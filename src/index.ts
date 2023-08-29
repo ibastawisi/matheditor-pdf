c765a67051fd437edf05649e7a8b3f1c74ad195a
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2023-08-29 20:47:39 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import puppeteer from '@cloudflare/puppeteer'
+
+interface Env {
+  MYBROWSER: Fetcher;
+}
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const browser = await puppeteer.launch(env.MYBROWSER);
+    const page = await browser.newPage()
+    const url = new URL(request.url);
+    const id = url.pathname.split("/")[2];
+    url.pathname = `/embed/${id}`;
+    await page.goto(url.toString(), { waitUntil: "networkidle0" })
+    const pdf = await page.pdf({
+      format: "A4",
+      printBackground: true,
+      margin: {
+        top: "0.4in",
+        right: "0.4in",
+        bottom: "0.4in",
+        left: "0.4in",
+      },
+    })
+    await browser.close()
+
+    try {
+      return new Response(pdf, {
+        headers: {
+          "Content-Type": "application/pdf",
+          "Content-Disposition": `inline; filename="${id}.pdf"`,
+        },
+        status: 200
+       })
+
+    } catch (e) {
+      return new Response('Something went wrong', { status: 500 })
+    }
+  }
+}
