06153d215726b9e5a809eb0c3b0199c6ff74c708
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2023-10-09 19:32:36 +0300

    add search params

--- src/index.ts
+++ src/index.ts
@@ -9,32 +9,38 @@ export default {
     const browser = await puppeteer.launch(env.MYBROWSER);
     const page = await browser.newPage()
     const url = new URL(request.url);
+    const search = url.searchParams;
+    const scale = search.get("scale") || "1";
+    const format = (search.get("format") || "A4") as puppeteer.PaperFormat;
+    const landscape = search.get("landscape") || "false";
     const id = url.pathname.split("/")[2];
     url.pathname = `/embed/${id}`;
     await page.goto(url.toString(), { waitUntil: "networkidle0" })
-    const pdf = await page.pdf({
-      format: "A4",
-      printBackground: true,
-      margin: {
-        top: "0.4in",
-        right: "0.4in",
-        bottom: "0.4in",
-        left: "0.4in",
-      },
-    })
-    const title = await page.title()
-    await browser.close()
-
     try {
+      const pdf = await page.pdf({
+        scale: Number(scale),
+        format,
+        landscape: landscape === "true",
+        printBackground: true,
+        margin: {
+          top: "0.4in",
+          right: "0.4in",
+          bottom: "0.4in",
+          left: "0.4in",
+        },
+      })
+      const title = await page.title()
+      await browser.close()
       return new Response(pdf, {
         headers: {
           "Content-Type": "application/pdf",
           "Content-Disposition": `inline; filename="${title}.pdf"`,
         },
         status: 200
-       })
-
+      })
     } catch (e) {
+      console.error(e);
+      await browser.close();
       return new Response('Something went wrong', { status: 500 })
     }
   }
